6b6164ed10badbb6255db5f4d31e382f94617b6f
icodeologist <denzilpinto01@gmail.com>
2026-03-13 08:11:56 +0530

    added: retry logic if the email provider fails using failedUserIDCHANNEL

--- backend/internal/worker/extractUserIDs.go
+++ backend/internal/worker/extractUserIDs.go
@@ -1,13 +1,17 @@
 package worker
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"math"
+	"time"
 
 	"github.com/icodeologist/disasterwatch/internal/db"
 	emailservice "github.com/icodeologist/disasterwatch/internal/email_service"
 	"github.com/icodeologist/disasterwatch/internal/models"
 	"github.com/icodeologist/disasterwatch/internal/utils"
+	"golang.org/x/time/rate"
 )
 
 func CallExtractUserIDWorker(reportChannel chan models.Report, affUsersIdChannel chan uint) {
@@ -27,6 +31,7 @@ func PrintInfo(affUsersIdChannel chan uint) {
 	}
 }
 
+// Get the users effected from the disasters and push their id to affuserchannel
 func StartExtractWorkers(n int, reportChannel <-chan models.Report, affUserIDChannel chan<- uint) {
 	// start n of workers
 	// TODO: add a recover defer function for each go routines spawned
@@ -35,19 +40,77 @@ func StartExtractWorkers(n int, reportChannel <-chan models.Report, affUserIDCha
 	}
 }
 
-func StartNotificationWorker(n int, affUsersIdChannel <-chan uint) {
-	fmt.Println("Started Notification Workers")
+type EmailrateLimiter struct {
+	limiter *rate.Limiter
+}
+type FailedEmailMessage struct {
+	UserId     uint
+	User       models.User
+	Status     string
+	Message    string
+	RetryTime  int
+	RetryDelay time.Duration
+}
+
+func StartNotificationWorker(n int, affUsersIdChannel <-chan uint, failedEmailsChan chan<- FailedEmailMessage) {
+	emailRateL := &EmailrateLimiter{
+		limiter: rate.NewLimiter(2, 5),
+	}
+
 	for i := 0; i < n; i++ {
-		go func(workerID int) {
-			var user models.User
+		go func(id int) {
 			for userID := range affUsersIdChannel {
+				var user models.User
 				if err := db.DB.Where("id=?", userID).First(&user).Error; err != nil {
 					log.Printf("DATABASE_ERR : %v\n", err)
 					continue
 				}
-				go emailservice.SendEmail(user.Email)
-				fmt.Println("Email send to USER : ", user.UserName, "USER EMAIL :", user.Email)
-				fmt.Println("WorkerID with :", workerID, "Sent Email to user :", user.UserName)
+				ctx := context.Background()
+
+				if err := emailRateL.limiter.Wait(ctx); err != nil {
+					log.Println("Error : ", err)
+					return
+				}
+				fmt.Println("Hello there")
+				err := emailservice.SendEmail(user.Email)
+				if err != nil {
+					failedMessage := &FailedEmailMessage{
+						UserId:    userID,
+						User:      user,
+						Status:    "not sent",
+						Message:   "Message",
+						RetryTime: 2,
+					}
+					failedEmailsChan <- *failedMessage
+				}
+
+			}
+		}(i)
+	}
+}
+
+func StartFailedEmailSendingWorker(n int, maxTries int, failedEmailsChan chan FailedEmailMessage, deadMessageChannel chan FailedEmailMessage) {
+	for i := 0; i < n; i++ {
+		go func(id int) {
+			for failedUserIDs := range failedEmailsChan {
+				timeTOwait := math.Pow(2, float64(failedUserIDs.RetryTime))
+				fMsg := &FailedEmailMessage{
+					User:       failedUserIDs.User,
+					UserId:     failedUserIDs.UserId,
+					Status:     "Email provider error",
+					Message:    "message",
+					RetryTime:  failedUserIDs.RetryTime + 1,
+					RetryDelay: time.Duration(timeTOwait),
+				}
+				if fMsg.RetryTime <= maxTries {
+					time.Sleep(time.Duration(timeTOwait) * time.Second)
+					err := emailservice.SendEmail(failedUserIDs.User.Email)
+					if err != nil {
+						failedEmailsChan <- *fMsg
+					}
+				} else {
+					deadMessageChannel <- *fMsg
+				}
 			}
 		}(i)
 	}
