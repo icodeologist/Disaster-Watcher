b7a8293d7195689b80398679bbf429114fd22580
Denzil Pinto <denzilpinto01@gmail.com>
2026-09-11 20:33:11 +0530

    refactor:remove duplicate job recovery functions

--- backend/internal/utils/recoverPendingJobs.go
+++ backend/internal/utils/recoverPendingJobs.go
@@ -2,52 +2,10 @@ package utils
 
 import (
 	"log"
-	"log/slog"
-	"time"
 
 	"github.com/icodeologist/disasterwatch/internal/db"
 	"github.com/icodeologist/disasterwatch/internal/models"
 )
-
-func RecoverPendingJobsFromDBOnStarting(verifcationChannel chan<- models.VerificationMessage) {
-	var pendingJobs []models.Jobs
-	if err := db.DB.Where("status=?", "pending").Find(&pendingJobs).Error; err != nil {
-		slog.Error("Db update error in RecoverPendingJobsFromDBOnStarting", "error", err)
-	}
-	if len(pendingJobs) > 0 {
-		for _, job := range pendingJobs {
-			vmsg := models.VerificationMessage{
-				JobID: job.Id,
-			}
-			job.Status = "processing"
-			err := db.DB.Save(&job).Error
-			if err != nil {
-				slog.Error("Db save error in RecoverPendingJobsFromDBOnStarting", "error", err)
-			}
-			verifcationChannel <- vmsg
-			slog.Info("Job restarted", "started_time", time.Now())
-		}
-	} else {
-		slog.Info("No pending jobs in DB")
-	}
-}
-func RecoverMidProcessingJobs(verifcationChannel chan<- models.VerificationMessage) {
-	var midProcessingJobs []models.Jobs
-	if err := db.DB.Where("status=?", "pending").Find(&midProcessingJobs).Error; err != nil {
-		slog.Error("failed to fetch the jobs with processing status", "error", err)
-	}
-	if len(midProcessingJobs) > 0 {
-		for _, job := range midProcessingJobs {
-			vmsg := models.VerificationMessage{
-				JobID: job.Id,
-			}
-			verifcationChannel <- vmsg
-			slog.Info("Job restarted", "started_time", time.Now())
-		}
-	} else {
-		slog.Info("No Processing jobs in DB")
-	}
-}
 
 func GetAllDONEJOBS() {
 	var job []models.Jobs
